785a1954d695598d25fedf974dcdcbeec33928de
littleTommyTan <982771996@qq.com>
2018-12-10 12:14:39 +0800

    datamodel update

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,7 +1,7 @@
-import BookingRecord from "./BookingRecord";
-import ConsultingRecord from "./ConsultingRecord";
-import Mutation from "./Mutation";
-import Query from "./Query";
+import BookingRecord from './BookingRecord';
+import ConsultingRecord from './ConsultingRecord';
+import Mutation from './Mutation';
+import Query from './Query';
 
 const resolvers = {
   Query,
