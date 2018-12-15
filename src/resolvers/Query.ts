db3455f5150af0aaf46b6eef9197c6ed2a0049ff
littleTommyTan <982771996@qq.com>
2018-12-15 18:05:11 +0800

    departments

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -8,6 +8,7 @@ const Query = {
   ads: async () => await prisma.ads(),
   adConsumptions: async () => await prisma.adConsumptionRecs(),
   consultationWorks: async () => await prisma.consultationWorks(),
-  admins: async () => await prisma.admins()
+  admins: async () => await prisma.admins(),
+  departments: async () => await prisma.departments()
 };
 export default Query;
