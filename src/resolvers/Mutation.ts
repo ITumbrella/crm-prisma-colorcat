70f88054d9ff44c4a18f516d0faac978e0aa96ac
littleTommyTan <982771996@qq.com>
2018-12-30 14:39:47 +0800

    update bill

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -348,6 +348,8 @@ const Mutation = {
   updateBill: async (parent, args, context) => {
     const bill = await prisma.bill({ id: args.id });
     const user = await prisma.bill({ id: args.id }).user();
+    console.log(user);
+
     if (user.balance < bill.totalPrice) throw "balance not enough";
     const editor = await Certify(context, {}, Identity.Editor);
     const retBill = await prisma.updateBill({
