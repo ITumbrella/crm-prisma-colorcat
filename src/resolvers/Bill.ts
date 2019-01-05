8e95b1deb9c2cc34676bbd063ef5ecb62f0a1804
littleTommyTan <982771996@qq.com>
2019-01-05 10:09:52 +0800

    bill bookingrecord

--- src/resolvers/Bill.ts
+++ src/resolvers/Bill.ts
@@ -2,7 +2,10 @@ import { prisma } from '../../generated/prisma-client';
 
 const Bill = {
   user: (parent, args, ctx) => prisma.bill({ id: parent.id }).user(),
-  billDetail: (parent, args, ctx) => prisma.bill({ id: parent.id }).billDetail()
+  billDetail: (parent, args, ctx) =>
+    prisma.bill({ id: parent.id }).billDetail(),
+  bookingRecord: (parent, args, ctx) =>
+    prisma.bill({ id: parent.id }).bookingRecord()
 };
 
 export default Bill;
