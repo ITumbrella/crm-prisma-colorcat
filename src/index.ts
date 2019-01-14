398cff8c26780e783bae9e3e863d80d81ef3776d
littleTommyTan <982771996@qq.com>
2019-01-15 00:14:09 +0800

    pkg

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,44 @@ import { GraphQLServer } from 'graphql-yoga';
 import { prisma } from '../generated/prisma-client';
 import resolvers from './resolvers';
 
-const server = new GraphQLServer({
-  typeDefs: "src/schema.graphql",
-  resolvers: resolvers,
-  context: req => ({ ...req, prisma })
-});
+var quickexec = (command, callback) => {
+  console.log(command);
+  require("child_process").exec(command, (error, stdout, stderr) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(null, stdout);
+    }
+  });
+};
 
-server.start(async () => {
-  console.log("CRMServer is running on http://localhost:4000");
+function exec(cmd: string) {
+  console.log(cmd);
+
+  require("child_process").execSync(cmd, { stdio: "inherit" });
+}
+
+exec("npm install yarn -g");
+exec("yarn");
+exec("yarn global add prisma@1.22.1");
+exec("docker-compose up -d");
+exec("prisma generate");
+quickexec("prisma deploy --force", (err, result) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(result);
+
+    if (result.trim() === "") return;
+
+    const server = new GraphQLServer({
+      typeDefs: "src/schema.graphql",
+      resolvers: resolvers,
+      context: req => ({ ...req, prisma })
+    });
+
+    server.start(async () => {
+      console.log("CRMServer is running on http://localhost:4000");
+    });
+  }
 });
--- tools/build.ts
+++ tools/build.ts
@@ -1,15 +1,13 @@
-import * as commander from 'commander';
-
-function exec(cmd: string) {
+export function exec(cmd: string) {
   console.log(cmd);
   require("child_process").execSync(cmd, { stdio: "inherit" });
 }
 
 const version = require("../package.json").version;
 
-commander.version(version);
-
 exec("npm install prisma@1.22.1 -g");
-exec("prisma deploy");
+exec("docker-compose up -d");
 exec("prisma generate");
-exec("npm ");
+setTimeout(() => {
+  exec("prisma deploy --force");
+}, 10000);
