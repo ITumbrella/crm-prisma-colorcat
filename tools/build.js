98bbf059b129e9ee7a6253f4bac4a358eca8e084
littleTommyTan <982771996@qq.com>
2019-01-15 01:17:44 +0800

    Update build.js

--- tools/build.js
+++ tools/build.js
@@ -1,29 +1,17 @@
-var quickexec = (command, callback) => {
-  console.log(command);
-  require("child_process").exec(command, (error, stdout, stderr) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(null, stdout);
-    }
-  });
-};
-
 function exec(cmd) {
   console.log(cmd);
-
   require("child_process").execSync(cmd, { stdio: "inherit" });
 }
 
+exec("docker-compose up -d");
+exec("npm install typescript -g");
+exec("npm install ts-node -g");
+exec("npm install pm2@latest -g");
+exec(`pm2 install typescript`);
 exec("npm install yarn -g");
 exec("yarn");
 exec("yarn global add prisma@1.22.1");
-exec("docker-compose up -d");
+exec("npm install typescript -g");
+exec(`pm2 start src/index.ts --name='crm-backend'`);
 exec("prisma generate");
-quickexec("prisma deploy --force", (err, result) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log(result);
-  }
-});
+exec("prisma deploy --force");
